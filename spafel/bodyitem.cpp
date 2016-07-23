5244863ac6aa3b4d284407c1b5679865827bdabd
Mathijs Romans <qt@romansland.nl>
2016-07-24 01:09:00 +0200

    added rotation

--- spafel/bodyitem.cpp
+++ spafel/bodyitem.cpp
@@ -2,6 +2,7 @@
 
 #include "body.h"
 #include "gravityscene.h"
+#include <QDebug>
 
 BodyItem::BodyItem(Body* body)
 : QGraphicsItem(),
@@ -21,4 +22,5 @@ BodyItem::update(const QRectF& tableRect, unsigned int time)
   const std::array<double, 4> state = m_body->getState(time);
   QPointF scenePos = GravityScene::envToScene(QPointF(state[0], state[1]), tableRect);
   setPos(scenePos.x(), scenePos.y());
+  setRotation(180*atan2(state[3], state[2])/M_PI);
 }
